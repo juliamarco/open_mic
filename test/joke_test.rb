c535fba57c05900415221a0cb026740fcf7c6fbc
Julia Marco <marcootj@gmail.com>
2018-12-05 09:12:35 -0700

    Adds iteration 1

---
+++ test/joke_test.rb
@@ -0,0 +1,37 @@
+require './lib/joke'
+require "minitest/autorun"
+require "minitest/pride"
+require 'pry'
+
+class JokeTest < Minitest::Test
+
+  def test_it_exists
+
+    joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+  assert_instance_of Joke, joke
+  end
+
+  def test_it_has_an_id
+
+    joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+  assert_equal 1, joke.id
+  end
+
+  def test_it_has_a_setup
+
+    joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+  assert_equal "Why did the strawberry cross the road?", joke.setup
+  end
+
+  def test_it_has_a_punchline
+
+    joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+  assert_equal "Because his mother was in a jam.", joke.punchline
+  end
+
+  
+end
