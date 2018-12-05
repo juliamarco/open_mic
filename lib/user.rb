861350d673fea854d6d5c38ebd07b2cf3b733591
Julia Marco <marcootj@gmail.com>
2018-12-05 09:42:58 -0700

    Adds iteration 3

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,6 @@
+require './lib/joke'
+
+
 class User
   attr_reader :name,
               :jokes
@@ -11,4 +14,24 @@ class User
     @jokes << joke
   end
 
+  def tell(user, joke)
+    user.learn(joke)
+  end
+
+  def joke_by_id(id)
+    @jokes.map do |joke|
+      if joke.id == id
+        return joke
+      end
+    end
+  end
+
 end
+
+sal = User.new("Sal")
+ali = User.new("Ali")
+joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+sal.tell(ali, joke_1)
+sal.tell(ali, joke_2)
+ali.joke_by_id(1)
--- test/user_test.rb
+++ test/user_test.rb
@@ -6,28 +6,28 @@ require 'pry'
 
 class UserTest < Minitest::Test
 
-  def test_it_exists
+  def test_user_exists
 
     sal = User.new("Sal")
 
   assert_instance_of User, sal
   end
 
-  def test_it_has_a_name
+  def test_user_has_a_name
 
     sal = User.new("Sal")
 
   assert_equal "Sal", sal.name
   end
 
-  def test_it_has_not_jokes_at_the_begginning
+  def test_user_do_not_know_jokes_at_the_begginning
 
     sal = User.new("Sal")
 
   assert_equal [], sal.jokes
   end
 
-  def test_it_can_learn_jokes
+  def test_user_can_learn_jokes
 
     sal = User.new("Sal")
     joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
@@ -36,6 +36,31 @@ class UserTest < Minitest::Test
     sal.learn(joke_2)
 
   assert_equal [joke_1, joke_2], sal.jokes
+  end
+
+  def test_user_can_tell_jokes_to_other_user
+
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    sal.tell(ali, joke_1)
+    sal.tell(ali, joke_2)
+
+  assert_equal [joke_1, joke_2], ali.jokes
+  end
+
+  def test_get_joke_by_id
+
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    sal.tell(ali, joke_1)
+    sal.tell(ali, joke_2)
+
+  assert_equal joke_1, ali.joke_by_id(1)
+  assert_equal joke_2, ali.joke_by_id(2)
   end
 
 
